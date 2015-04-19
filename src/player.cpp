49af02adbee10ff12b40c92f8519a95b5aff34f4
Ranieri Althoff <ranisalt@gmail.com>
2015-04-19 03:47:18 -0300

    Make player walkthrough optional

--- src/player.cpp
+++ src/player.cpp
@@ -829,7 +829,7 @@ bool Player::canWalkthrough(const Creature* creature) const
 	}
 
 	const Player* player = creature->getPlayer();
-	if (!player) {
+	if (!player || g_config.getBoolean(ConfigManager::ALLOW_WALKTHROUGH)) {
 		return false;
 	}
 
@@ -865,7 +865,7 @@ bool Player::canWalkthroughEx(const Creature* creature) const
 	}
 
 	const Player* player = creature->getPlayer();
-	if (!player) {
+	if (!player || g_config.getBoolean(ConfigManager::ALLOW_WALKTHROUGH)) {
 		return false;
 	}
 
