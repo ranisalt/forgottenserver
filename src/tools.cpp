66986061e3b916958fe286f698392c28424b3041
Ranieri Althoff <ranisalt@gmail.com>
2021-03-14 17:20:03 +0100

    Use fmt instead of C to format in tools

--- src/tools.cpp
+++ src/tools.cpp
@@ -19,8 +19,10 @@
 
 #include "otpch.h"
 
-#include "tools.h"
 #include "configmanager.h"
+#include "tools.h"
+#include <fmt/chrono.h>
+#include <fmt/format.h>
 
 extern ConfigManager g_config;
 
@@ -348,44 +350,17 @@ void trimString(std::string& str)
 
 std::string convertIPToString(uint32_t ip)
 {
-	char buffer[17];
-
-	int res = sprintf(buffer, "%u.%u.%u.%u", ip & 0xFF, (ip >> 8) & 0xFF, (ip >> 16) & 0xFF, (ip >> 24));
-	if (res < 0) {
-		return {};
-	}
-
-	return buffer;
+	return fmt::format("{:d}.{:d}.{:d}.{:d}", ip & 0xFF, (ip >> 8) & 0xFF, (ip >> 16) & 0xFF, (ip >> 24));
 }
 
 std::string formatDate(time_t time)
 {
-	const tm* tms = localtime(&time);
-	if (!tms) {
-		return {};
-	}
-
-	char buffer[20];
-	int res = sprintf(buffer, "%02d/%02d/%04d %02d:%02d:%02d", tms->tm_mday, tms->tm_mon + 1, tms->tm_year + 1900, tms->tm_hour, tms->tm_min, tms->tm_sec);
-	if (res < 0) {
-		return {};
-	}
-	return {buffer, 19};
+	return fmt::format("{:%d-%m-%Y %H:%M:%S}", fmt::localtime(time));
 }
 
 std::string formatDateShort(time_t time)
 {
-	const tm* tms = localtime(&time);
-	if (!tms) {
-		return {};
-	}
-
-	char buffer[12];
-	size_t res = strftime(buffer, 12, "%d %b %Y", tms);
-	if (res == 0) {
-		return {};
-	}
-	return {buffer, 11};
+	return fmt::format("{:%d %b %Y}", fmt::localtime(time));
 }
 
 Direction getDirection(const std::string& string)
