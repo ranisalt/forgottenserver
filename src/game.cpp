4d7ce2dcd543873120ce54644cc9ff128663af0b
Ranieri Althoff <ranisalt@gmail.com>
2024-05-12 20:40:25 +0200

    Change check creatures to use vector

--- src/game.cpp
+++ src/game.cpp
@@ -3878,7 +3878,8 @@ void Game::checkCreatures(size_t index)
 			++it;
 		} else {
 			creature->inCheckCreaturesVector = false;
-			it = checkCreatureList.erase(it);
+			std::iter_swap(it, --end);
+			checkCreatureList.pop_back();
 			ReleaseCreature(creature);
 		}
 	}
