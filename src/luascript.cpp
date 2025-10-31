c6be1a68db3981513d4f58b4b5a0c98738237d32
Ranieri Althoff <ranisalt@gmail.com>
2025-10-31 04:14:16 +0100

    Final fixes with clang-tidy

--- src/luascript.cpp
+++ src/luascript.cpp
@@ -11926,7 +11926,7 @@ int LuaScriptInterface::luaGuildGetMembersOnline(lua_State* L)
 	lua_createtable(L, members.size(), 0);
 
 	int index = 0;
-	for (auto player : members) {
+	for (const auto& player : members) {
 		tfs::lua::pushSharedPtr(L, player);
 		tfs::lua::setMetatable(L, -1, "Player");
 		lua_rawseti(L, -2, ++index);
@@ -12735,7 +12735,7 @@ int LuaScriptInterface::luaHouseGetTiles(lua_State* L)
 	lua_createtable(L, tiles.size(), 0);
 
 	int index = 0;
-	for (auto tile : tiles) {
+	for (const auto& tile : tiles) {
 		tfs::lua::pushSharedPtr(L, tile);
 		tfs::lua::setMetatable(L, -1, "Tile");
 		lua_rawseti(L, -2, ++index);
@@ -12756,7 +12756,7 @@ int LuaScriptInterface::luaHouseGetItems(lua_State* L)
 	lua_newtable(L);
 
 	int index = 0;
-	for (auto tile : tiles) {
+	for (const auto& tile : tiles) {
 		TileItemVector* itemVector = tile->getItemList();
 		if (itemVector) {
 			for (const auto& item : *itemVector) {
@@ -16041,7 +16041,7 @@ int LuaScriptInterface::luaPartyGetInvitees(lua_State* L)
 		lua_createtable(L, party->getInvitationCount(), 0);
 
 		int index = 0;
-		for (auto player : party->getInvitees()) {
+		for (const auto& player : party->getInvitees()) {
 			tfs::lua::pushSharedPtr(L, player);
 			tfs::lua::setMetatable(L, -1, "Player");
 			lua_rawseti(L, -2, ++index);
