e8437ff7fc52c28c2c5652f5ede17d4a67eae693
Ranieri Althoff <ranisalt@gmail.com>
2025-12-16 17:18:32 -0300

    Move ping-pong storage keys to storages.lua

--- data/scripts/lib/ping_pong.lua
+++ data/scripts/lib/ping_pong.lua
@@ -1,18 +1,15 @@
-local LAST_PING_STORAGE = 7464
-local LAST_PONG_STORAGE = 7664
-
 function Player.getLastPing(self)
-  return self:getStorageValue(LAST_PING_STORAGE) or 0
+  return self:getStorageValue(PlayerStorageKeys.lastPing) or 0
 end
 
 function Player.setLastPing(self, time)
-  self:setStorageValue(LAST_PING_STORAGE, time)
+  self:setStorageValue(PlayerStorageKeys.lastPing, time)
 end
 
 function Player.getLastPong(self)
-  return self:getStorageValue(LAST_PONG_STORAGE) or 0
+  return self:getStorageValue(PlayerStorageKeys.lastPong) or 0
 end
 
 function Player.setLastPong(self, time)
-  self:setStorageValue(LAST_PONG_STORAGE, time)
+  self:setStorageValue(PlayerStorageKeys.lastPong, time)
 end
