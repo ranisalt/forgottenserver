7088984d7e8d77ad7ef49f187d5f851cfc839710
Ranieri Althoff <ranisalt@gmail.com>
2025-12-23 22:48:36 -0300

    fix(lua): swap luaSharedPtrDelete and getSharedPtr implementations

--- src/lua/api.h
+++ src/lua/api.h
@@ -116,17 +116,17 @@ T* getUserdata(lua_State* L, int32_t arg)
 
 // SharedPtr helpers
 template <class T>
+std::shared_ptr<T>& getSharedPtr(lua_State* L, int32_t arg)
+{
+	return *static_cast<std::shared_ptr<T>*>(lua_touserdata(L, arg));
+}
+
+template <class T>
 int luaSharedPtrDelete(lua_State* L)
 {
 	auto& ptr = tfs::lua::getSharedPtr<T>(L, 1);
 	std::destroy_at(std::addressof(ptr));
 	return 0;
-}
-
-template <class T>
-std::shared_ptr<T>& getSharedPtr(lua_State* L, int32_t arg)
-{
-	return *static_cast<std::shared_ptr<T>*>(lua_touserdata(L, arg));
 }
 
 template <class T>
